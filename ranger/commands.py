69192835a7f5b41f9fa02f284c893999c182d082
Reefufui <reefufui@gmail.com>
2022-11-02 17:22:33 +0300

    Minor changes.

--- ranger/commands.py
+++ ranger/commands.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 class set_as_wallpaper(Command):
     """:set_as_wallpaper <filename>
 
-    A simple command that sets image as a wallpaper using 'feh'.
+    Sets image as a wallpaper using 'feh'.
     """
     def execute(self):
         if self.arg(1):
@@ -24,13 +24,53 @@ class set_as_wallpaper(Command):
         else:
             target_filename = self.fm.thisfile.path
 
-        self.fm.notify("Let's edit the file " + target_filename + "!")
+        self.fm.notify("Setting " + target_filename + " as wallpaper!")
 
         if not os.path.exists(target_filename):
             self.fm.notify("The given file does not exist!", bad=True)
             return
 
         self.fm.run("feh --bg-fill " + target_filename)
+
+class dye(Command):
+    """:dye <palette>
+
+    Make image's copy that matches given color palette
+    """
+    def execute(self):
+        target_filename = self.fm.thisfile.path
+        target_palette = self.rest(1) if self.arg(1) else "catpuccin"
+
+        self.fm.notify("Dying " + target_filename + " to match " + target_palette + " palette!")
+
+        if not os.path.exists(target_filename):
+            self.fm.notify("The given file does not exist!", bad=True)
+            return
+
+        p = Path(target_filename)
+        new_filename = "{}/{}_{}".format(p.parent, p.stem, target_palette)
+
+        self.fm.run("dye " + target_filename + " -p " + target_palette + " -o " + new_filename + " -b")
+
+class convert(Command):
+    """:convert <ext>
+
+    Convert image using imagick's convert.
+    """
+    def execute(self):
+        target_filename = self.fm.thisfile.path
+        target_ext = self.rest(1) if self.arg(1) else "png"
+
+        self.fm.notify("Converting " + target_filename + " to " + target_ext + "!")
+
+        if not os.path.exists(target_filename):
+            self.fm.notify("The given file does not exist!", bad=True)
+            return
+
+        p = Path(target_filename)
+        new_filename = "{}/{}.{}".format(p.parent, p.stem, target_ext)
+
+        self.fm.run("convert " + target_filename + " " + new_filename)
 
 @register_image_displayer("mpv")
 class MPVImageDisplayer(ImageDisplayer):
